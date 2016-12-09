3529f8b03694abec836fed85922d6090d5b76e7c
Veljko Vujcic <sambolino@gmail.com>
2016-12-09 17:38:22 +0100

    write alerts in json format (according to avro schema) to files in 'json_output' directory

--- python/lsst/sims/alertsim/avro_utils.py
+++ python/lsst/sims/alertsim/avro_utils.py
@@ -14,7 +14,7 @@ def _raise_no_avro(method_name):
     raise RuntimeError(msg)
 
 
-def catsim_to_avro(list_of_query_dicts, schemaURI='avsc/diasource.avsc'):
+def catsim_to_avro(list_of_query_dicts, session_dir, schemaURI='avsc/diasource.avsc'):
 
     """
     Input: list of dictionaries for a catsim visit which consists
@@ -37,15 +37,31 @@ def catsim_to_avro(list_of_query_dicts, schemaURI='avsc/diasource.avsc'):
     writing_time = timer()
     writer = DataFileWriter(open("avsc/alert.avro", "wb"), DatumWriter(), alert_schema)
 
-    for qd in list_of_query_dicts:
+    print "number of events %d" % (len(list_of_query_dicts))
 
+    for qd in list_of_query_dicts:
+        print "Ccd and visit id %d" % (qd['ccdVisitId'])
+        
+        #last 4 digits of ccdVisitId are chip number
+        chipNum = str(qd['ccdVisitId'])[-4:]
+        #open file with a name of a chipNum in append mode
+        file_obj = open("json_output/"+session_dir+"/"+chipNum, 'a')
+
+        alert_dict = qd
+        #alert_dict.pop("ssObjectId", None)
         alert_dict = {'alertID':45135, 'l1dbID':12545, 'diaSource':qd, 'prevDiaSources':[qd]*30}
         #alert_dict = {'alertID':45135, 'l1dbID':12545, 'diaSource':qd}
         json_qd = json.loads(json.dumps(alert_dict))
-        print json_qd
+        #file_obj.write(json_qd)
+        json.dump(json_qd, file_obj)
+        #print alert_dict
+
         writer.append(json_qd)
+        file_obj.close()
+
 
     writer.close()
+    #file_obj.close()
     """
     print "writing time %s" % (timer() - writing_time)
 
@@ -58,7 +74,7 @@ def catsim_to_avro(list_of_query_dicts, schemaURI='avsc/diasource.avsc'):
 
     #print "reading time %s" % (timer() - reading_time)
 
-    reader.close()
+    #reader.close()
 
 def load_avsc_schema(schema_path, names = None):
     
